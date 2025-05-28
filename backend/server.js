ad76f9e06d272c5329ee2767f95248879e1973f7
Sérgio Soares <sergiosoares@Mac-mini-de-Sergio.local>
2025-05-28 18:21:05 +0100

    preparado para deploy

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
-
+// const cors = require("cors");
 const userRoutes = require("./routes/user");
-
 const ticketsRoutes = require("./routes/tickets");
-const messageRoutes = require("./routes/messages");
+//const messageRoutes = require("./routes/messages");
+const path = require("path");
 
 // express app
 const app = express();
@@ -14,21 +14,37 @@ const app = express();
 // middleware
 app.use(express.json());
 
+// if (process.env.NODE_ENV === "development") {
+//   app.use(
+//     cors({
+//       origin: "http://localhost:3000",
+//     })
+//   );
+// }
+
+// if (process.env.NODE_ENV !== "production") {
+//   app.use(cors());
+// }
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // routes
-
 app.use("/api/user", userRoutes);
-
-// rota de testes
+// rota de tickets
 app.use("/api/tickets", ticketsRoutes);
-
 // mensagens
+//app.use("/api/messages", messageRoutes);
 
-app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/", "build", "index.html"));
+  });
+}
 
 // connect to db
 mongoose
